5fea740ec5a2cae7931cc4a028197d46ea413b9a
Object Press <vincent@objectpress.io>
2021-09-06 00:49:02 -0400

    gatsby-plugin-objectpress

--- gatsby-graphql/gatsby-config.js
+++ gatsby-graphql/gatsby-config.js
@@ -29,5 +29,13 @@ module.exports = {
         icon: `${__dirname}/src/assets/images/logo-icon.png`,
       },
     },
+    // loads the source-plugin and adds credentials
+    {
+      resolve: `gatsby-plugin-objectpress`,
+      options: {
+        appSecret: `XtMsuJ+nr7PJ4wqxWjRUhqwbg7BuCqHOm2Q5kP5Xq4M=`,
+        userSecret: `gcMeOyAu8TOYcDBBFkbNFS4tiTubU0ow+6IEoGcl3gQ=`,
+      },
+    },
   ],
 };
--- gatsby-graphql/gatsby-node.js
+++ gatsby-graphql/gatsby-node.js
@@ -1,41 +1,4 @@
 const { slugify } = require("./src/utils");
-const axios = require("axios");
-
-const POST_NODE_TYPE = `Post`;
-
-exports.sourceNodes = async ({
-  actions,
-  createContentDigest,
-  createNodeId,
-}) => {
-  const { createNode } = actions;
-  // Object Press credentials
-  const creds = {
-    scan: `apps`,
-    appSecret: `XtMsuJ+nr7PJ4wqxWjRUhqwbg7BuCqHOm2Q5kP5Xq4M=`,
-    userSecret: `gcMeOyAu8TOYcDBBFkbNFS4tiTubU0ow+6IEoGcl3gQ=`,
-  };
-
-  // Call the external API endpoint to get all posts
-  const response = await axios.post(`https://v2.objectpress.io/content`, creds);
-
-  // Gather the response data
-  const content = response.data;
-
-  // loop through data and create Gatsby nodes
-  content.body.forEach((post) =>
-    createNode({
-      id: createNodeId(`${POST_NODE_TYPE}-${post.title}`),
-      ...post,
-      internal: {
-        type: POST_NODE_TYPE,
-        content: JSON.stringify(post),
-        contentDigest: createContentDigest(post),
-      },
-    })
-  );
-  return;
-};
 
 exports.createPages = async ({ actions: { createPage }, graphql }) => {
   const { data } = await graphql(`
